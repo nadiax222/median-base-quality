67d39f27d631c3ecb77562fd5ae58ddc7bd79992
Nadia Cichocka <nadia.cichocka05.com>
2026-05-09 13:04:01 +0200

    added code!

--- mediana_jakości_zasad.py
+++ mediana_jakości_zasad.py
@@ -1,3 +1,4 @@
+#odbliczanie mediany
 import statistics
 with open ("file.fq") as f: 
     lines = f.read().splitlines()
@@ -5,4 +6,9 @@ quality_line = [3]
 qualities = [ord(x) - 33 for x in quality_line]
 print(statistics.median(qualities))
 #ord -33 zmienia znak jakości na wartość phred
-
+#spodziewana liczba błędów
+first_20 = qualities[:20]
+last_20 = qualities[-20:]
+errors_first = sum([10**(-q/10) for q in first_20])
+errors_last = sum([10**(-q/10) for q in last_20])
+print(f"Spodziewana liczba błędów w pierwszych 20 znakach: {errors_first}")
