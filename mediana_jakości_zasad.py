504571b6cbde1cdaafe6c28547c560da44878b3a
Nadia Cichocka <nadia.cichocka05.com>
2026-05-09 13:07:08 +0200

    added code on extracting barcodes

--- mediana_jakości_zasad.py
+++ mediana_jakości_zasad.py
@@ -12,3 +12,7 @@ last_20 = qualities[-20:]
 errors_first = sum([10**(-q/10) for q in first_20])
 errors_last = sum([10**(-q/10) for q in last_20])
 print(f"Spodziewana liczba błędów w pierwszych 20 znakach: {errors_first}")
+
+#wyciąganie barkodów
+barcode = header.split("#")[1].split("/")[0] #1 bierze część po #, split("/")[0] bierze część przed / usuwa /1 i zostawia sam indeks
+
